6ec857801a473d6816d7b6b7c54c1248921e1f54
labuser <labuser@ip-172-31-8-193.ap-south-1.compute.internal>
2026-01-07 17:21:21 +0000

    Auth-service Code Commited

---
+++ src/main/java/com/auth_service/dto/LoginDto.java
@@ -0,0 +1,21 @@
+package com.auth_service.dto;
+
+import jakarta.validation.constraints.NotNull;
+
+public class LoginDto {
+	@NotNull
+    private String email;
+	private String password;
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
