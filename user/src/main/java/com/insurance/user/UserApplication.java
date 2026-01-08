1f303daadcc78bca6f063da6952ce535c10d8542
rithun17 <rithunwork@gmail.com>
2026-01-08 04:16:04 +0000

    Enable service discovery for UserApplication

--- user/src/main/java/com/insurance/user/UserApplication.java
+++ user/src/main/java/com/insurance/user/UserApplication.java
@@ -2,7 +2,8 @@ package com.insurance.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+@EnableDiscoveryClient
 @SpringBootApplication
 public class UserApplication {
 
